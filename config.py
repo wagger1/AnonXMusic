3f4bb114e6843c638ba72af21c066fbd0a01ad57
Akhil.A.S <119261400+wagger1@users.noreply.github.com>
2025-07-14 12:06:24 +0530

    Update config.py
    Signed-off-by: Akhil.A.S <119261400+wagger1@users.noreply.github.com>

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonX
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/CxMaxTg")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CxMaxTg")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/EvaSprt")
 
